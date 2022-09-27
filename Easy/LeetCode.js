137a61de024d9628823290d09aa5558843772830
Axe714 <axearzadon@gmail.com>
2022-09-27 09:02:42 -0700

    Added solution to valid parentheses

--- Easy/LeetCode.js
+++ Easy/LeetCode.js
@@ -70,9 +70,9 @@ var twoSum = function(nums, target) {
 // s consists of parentheses only '()[]{}'.
 
 
-// MY ANSWER
+// MY ANSWER (WRONG...)
 
-const isValid = (input) => {
+const isValidWrong = (input) => {
     // // Initialise an array to store the closing brackets expected
     let validString = [];
     
@@ -94,4 +94,32 @@ const isValid = (input) => {
         
         return true
     }
+}
+
+
+// SOLUTION..
+
+var isValid = function (s) {
+    // Stack to store left symbols
+    const leftSymbols = [];
+    // Loop for each character of the string
+    for (let i = 0; i < s.length; i++) {
+        // If left symbol is encountered
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            leftSymbols.push(s[i]);
+        }
+        // If right symbol is encountered
+        else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '(') {
+            leftSymbols.pop();
+        } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '{') {
+            leftSymbols.pop();
+        } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length - 1] === '[') {
+            leftSymbols.pop();
+        }
+        // If none of the valid symbols is encountered
+        else {
+            return false;
+        }
+    }
+    return leftSymbols.length === 0;
 }
